180ca54cf678826191dacd8be1618132ef1c670d
Thinu <88131481+Rameshthinushan@users.noreply.github.com>
2024-12-27 23:01:22 +0530

    ui design

--- src/pages/technical-officer/dashboard.jsx
+++ src/pages/technical-officer/dashboard.jsx
@@ -1,8 +1,80 @@
+
+import Select from 'react-select'
+
 const DashBoard = () => {
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ]
+  const find = (e) => {
+    console.log('adv', e)
+  }
+
+  const today = new Date().toJSON().slice(0, 10);
   return (
     <div className="row">
       <div className="col">
-        cdc
+
+        <div className="row">
+          <div className="col text-end mt-3 mb-3">
+            <button class="btn btn-sm site-btn me-2">
+              <i className="bi bi-plus-circle-fill me-2"></i>
+              Create New Task
+            </button>
+            <button class="btn btn-sm btn-secondary">
+              <i className="bi bi-funnel-fill me-2"></i>
+              Filter
+            </button>
+          </div>
+        </div>
+        
+        <div className="row">
+          <div className="col">
+            <ul className="list-unstyled">
+              <li className="border-bottom fw-medium pb-1 pt-1 small">
+                <div className="row">
+                  <div className="col-9">Task Details (<span className='small'>{today}</span>)</div>
+                  <div className="col-3">Action</div>
+                </div>
+              </li>
+              {Array.from({ length: 12 }).map((_, index) => (
+                <li
+                  key={index} 
+                  className="border-bottom pb-1 pt-1 small"
+                >
+                  <div className="row align-items-center">
+                    <div className="col-9">
+                      <div className="fw-medium">Excavation</div>
+                      <div className="small">Lorem Ipsum is simply dummy text of the printing and typesetting industry</div>
+                    </div>
+                    <div className="col-3">
+                      <div className="d-flex">
+                        <div className="me-2">
+                          <button className="btn btn-sm btn-warning">
+                            <i className="bi bi-pencil-fill"></i>
+                          </button>
+                        </div>
+                        <div className="">
+                          <button className="btn btn-sm btn-danger">
+                            <i className="bi bi-trash"></i>
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              ))}
+   
+            </ul>
+          </div>
+        </div>
+
+        {/* <Select 
+          options={options} 
+          placeholder="Select an option" 
+          onChange={(e) => find(e.target)}
+        /> */}
       </div>
     </div>
   )
--- src/pages/technical-officer/layout.jsx
+++ src/pages/technical-officer/layout.jsx
@@ -28,25 +28,34 @@ const Layout = () => {
 const NavMobile = () => {
   return (
     <div className="row">
-      <div className="col text-bg-dark pt-1 pb-1">
-        <div className="row align-items-center">
+      <div className="col nab-bg pt-2 pb-2">
+        <div className="row">
           <div className="col">
-            <button className="menu-btn">
-              <i className="bi bi-text-left fs-4"></i>
-            </button>
-          </div>
-          <div className="col fw-medium nav-text-color">Task</div>
-          <div className="col">
-            <div className="d-flex justify-content-end align-items-center">
+            <div class="d-flex align-items-center">
               <div className="me-2">
+                <span className='nav-text-name'>RT</span>
+              </div>
+              <div className="small">
+                <div className="fw-medium">Thinushan</div>
+                <div className="small">thinushan@gmail.com</div>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="d-flex justify-content-end">
+              <div className="me-3">
                 <button className="mobile-nav-btn">
                   <i className="bi bi-bell fs-4"></i>
                 </button>
               </div>
-              <div className="nav-text-name me-2">RT</div>
-              <div>
+              <div className="me-3">
                 <button className="mobile-nav-btn">
-                  <i class="bi bi-box-arrow-right fs-4"></i>
+                  <i className="bi bi-box-arrow-right fs-4"></i>
+                </button>
+              </div>
+              <div>
+                <button className="menu-btn">
+                  <i className="bi bi-text-left fs-4"></i>
                 </button>
               </div>
             </div>
