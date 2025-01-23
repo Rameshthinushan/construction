8ce827650a1bb3db1d139635cbaf09dca23ed1b4
Thinu <88131481+Rameshthinushan@users.noreply.github.com>
2025-01-23 23:27:44 +0530

    dashboard working

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -78,10 +78,10 @@ const DashboardDeskTop = () => {
           {/* content  panel */}
           <div className="col-11">
             {/* top navigation panel */}
-            <div className="row">
+            <div className="row mt-3 mb-3 ms-2 me-2 align-items-center">
               {/* quick navigation panel */}
               <div className="col-lg-8">
-                <div className="site-bg-compo m-4 p-3 rounded-2 site-border">
+                <div className="site-bg-compo p-3 rounded-2 site-border">
                   <div className="row nav-font-color fw-medium text-center align-items-center">
                     <div className="col">
                       <div className="active-quick-nav p-2 quick-nav-item">Quick nav 1</div>
@@ -111,14 +111,14 @@ const DashboardDeskTop = () => {
               </div>
               {/* customer Details */}
               <div className="col-lg-4">
-                <div className="m-4 p-3">
+                <div className="m-4">
                   <div className="align-items-center d-flex justify-content-end">
                     <div className="me-4 dec-font-color"><i className="bi bi-bell fs-4"></i></div>
                     <div className="">
                       <div className="d-flex align-items-center">
                         <div className="small">
                           <div>Thinusan</div>
-                          <div className="small">Thinushan@gmail.com</div>
+                          <div className="small nav-font-color">Thinushan@gmail.com</div>
                         </div>
                         <div className="ms-4">
                           <span className="fw-medium p-2 rounded-2 site-bg-compo site-border">RT</span>
@@ -131,29 +131,119 @@ const DashboardDeskTop = () => {
               </div>
             </div>
 
-            <div className="row">
+            <div className="row ms-2 me-2">
               <div className="col"> 
                 {/* content */}
                 <div className="row">
                   <div className="col">
-                    <div className="site-bg-compo m-4 p-3 rounded-2 site-border h-100"></div>
+                    <div className="site-bg-compo p-3 rounded-2 site-border h-100">
+                      <div>
+                        <div class="text-end nav-font-color small mb-2">2025-01-01 <span className="fw-bold text-white ms-2 me-2">To</span> 2025-01-23</div>
+                        <div className="d-flex align-items-end site-border-bottom pb-2">
+                          <div className="w-100">
+                            <h6 className="mb-3">Heloooo WelcomeBack Mr.Thinu 😎</h6>
+                            <h5>Your Company</h5>
+                            <h1 className="dec-font-color">Overview Breakdown</h1>
+                          </div>
+                          <div>
+                            <img src="assets/site-images/rb_2148887079.png" alt="" height="140" className="zoom"/>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="d-flex justify-content-between mt-3">
+                        <div>
+                          <div className="nav-font-color small">Revenue</div>
+                          <div className="fw-bold fs-5">126,000,000.00</div> 
+                        </div>
+                        <div>
+                          <div className="nav-font-color small"> Complete</div>
+                          <div className="fw-bold fs-5">20</div>
+                        </div>
+                        <div>
+                          <div className="nav-font-color small"> Ongoing </div>
+                          <div className="fw-bold fs-5">10</div>
+                        </div>
+                        <div>
+                          <div className="nav-font-color small">Year over Year Ratio</div>
+                          <div className="fw-bold fs-5"><span>+ 100,000.00</span> <span className="ms-4">20%<i className="bi bi-arrow-up"></i></span></div>
+                        </div>
+                      </div>
+                      <div className="text-end mt-3"><i className="bi bi-arrow-right"></i></div>
+                    </div>
                   </div>
                   <div className="col">
-                    <div className="row">
+                    <div className="row mb-4">
                       <div className="col">
-                        <div className="site-bg-compo m-4 p-3 rounded-2 site-border h-100"></div>
+                        <div className="site-bg-compo pt-3 pb-3 ps-4 pe-4 rounded-2 site-border h-100 zoom">
+                          <div className="d-flex">
+                            <div className="w-100">
+                              <div className="nav-font-color">Ongoing Projects</div>
+                              <div><h1>10</h1></div>
+                            </div>
+                            <div>
+                              <img src="assets/site-images/contruction.png" alt="" height="80"/>
+                            </div>
+                          </div>
+                          <div className="text-end mt-3"><i className="bi bi-arrow-right"></i></div>
+                        </div>
                       </div>
                       <div className="col">
-                        <div className="site-bg-compo m-4 p-3 rounded-2 site-border h-100"></div>
+                        <div className="site-bg-compo pt-3 pb-3 ps-4 pe-4 rounded-2 site-border h-100 zoom">
+                          <div className="d-flex">
+                            <div className="w-100">
+                              <div className="nav-font-color">Ongoing Projects</div>
+                              <div><h1>10</h1></div>
+                            </div>
+                            <div>
+                              <img src="assets/site-images/contruction.png" alt="" height="80"/>
+                            </div>
+                          </div>
+                          <div className="text-end mt-3"><i className="bi bi-arrow-right"></i></div>
+                        </div>
                       </div>
                     </div>
+
                     <div className="row">
                       <div className="col">
-                        <div className="site-bg-compo m-4 p-3 rounded-2 site-border h-100"></div>
+                        <div className="site-bg-compo pt-3 pb-3 ps-4 pe-4 rounded-2 site-border h-100 zoom">
+                          <div className="d-flex">
+                            <div className="w-100">
+                              <div className="nav-font-color">Ongoing Projects</div>
+                              <div><h1>10</h1></div>
+                            </div>
+                            <div>
+                              <img src="assets/site-images/contruction.png" alt="" height="80"/>
+                            </div>
+                          </div>
+                          <div className="text-end mt-3"><i className="bi bi-arrow-right"></i></div>
+                        </div>
                       </div>
                       <div className="col">
-                        <div className="site-bg-compo m-4 p-3 rounded-2 site-border h-100"></div>
+                        <div className="site-bg-compo pt-3 pb-3 ps-4 pe-4 rounded-2 site-border h-100 zoom">
+                          <div className="d-flex">
+                            <div className="w-100">
+                              <div className="nav-font-color">Ongoing Projects</div>
+                              <div><h1>10</h1></div>
+                            </div>
+                            <div>
+                              <img src="assets/site-images/contruction.png" alt="" height="80"/>
+                            </div>
+                          </div>
+                          <div className="text-end mt-3"><i className="bi bi-arrow-right"></i></div>
+                        </div>
                       </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="row mt-4">
+                  <div className="col">
+                    <div className="site-bg-compo p-3 rounded-2 site-border h-100 text-center">
+                      Load More...
+                    </div>
+                  </div>
+                  <div className="col">
+                    <div className="site-bg-compo p-3 rounded-2 site-border h-100 text-center">
+                      Load More...
                     </div>
                   </div>
                 </div>
