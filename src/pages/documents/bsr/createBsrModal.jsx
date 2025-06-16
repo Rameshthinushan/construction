65d9bb0f55ed6c17d7f11a4a62d7f80b0a617997
Thinu <88131481+Rameshthinushan@users.noreply.github.com>
2025-06-16 09:39:24 +0530

    Update createBsrModal.jsx

--- src/pages/documents/bsr/createBsrModal.jsx
+++ src/pages/documents/bsr/createBsrModal.jsx
@@ -15,7 +15,7 @@ const CreateBsrModal = ({ show, handleClose, unitsData }) => {
   });
 
   const [validate, setValidate] = useState(false);
-
+  const [materialCount, SetMaterialCount] = useState(1);
   const options = unitsData.map((unit) => ({
     value: String(unit.id),
     label: unit.name
@@ -29,58 +29,24 @@ const CreateBsrModal = ({ show, handleClose, unitsData }) => {
     }));
   };
 
-  const handleUnitChange = (selectedOption) => {
-    setFormData((prev) => ({
-      ...prev,
-      unit: selectedOption ? selectedOption.value : ''
-    }));
-  };
 
-  const createNewTool = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    setValidate(true);
 
-    // Example: basic front-end validation
-    if (formData.tool_name && formData.unit) {
-      Request({
-        url: '/create-tool',
-        body: formData,
-      }).then ((res) => {
-        if (res.message) {
-          const rate =  res.tool_rate.rate
-          const obj = {
-            "tool_code": res.tool_rate.tool_code,
-            "tool_name":  res.tool_rate.tool_name,
-            "rates": {
-              [rate]: res.tool_rate.unit
-            },
-            "id": res.tool_rate.id
-          }
-          console.log(obj)
-          dispatch(setNewTool(obj));
-          handleClose();
-        }
-      }).catch(() => {
-        
-      })
-    }
-  };
+  
 
   return (
-    <Modal show={show} onHide={handleClose} size="md">
+    <Modal 
+      show={show} 
+      onHide={handleClose} 
+      fullscreen={true}
+    >
       <Modal.Header closeButton className="border-0" />
       <Modal.Body>
         <div className="row">
           <div className="col">
-            <form
-              onSubmit={createNewTool}
-              className={`needs-validation ${validate ? 'was-validated' : ''}`}
-              noValidate
-            >
-              <div className="row mb-3">
-                <div className="col-3 fw-medium">Bsr code:</div>
-                <div className="col text-secondary">
+            <form className="">
+              <div className="row mb-3 align-items-center">
+                <div className="col-lg-1 fw-medium">Bsr code:</div>
+                <div className="col-lg-3 text-secondary">
                   <input
                     type="text"
                     className="form-control form-control-sm p-2"
@@ -93,54 +59,179 @@ const CreateBsrModal = ({ show, handleClose, unitsData }) => {
                   <div className="invalid-feedback">Please enter a bsr code.</div>
                 </div>
               </div>
-
-              <div className="row mb-3">
-                <div className="col">
+              <div className="row mb-3 align-items-center">
+                <div className="col-lg-1 fw-medium">Bsr Unit:</div>
+                <div className="col-lg-3 text-secondary">
+                    <Select 
+                      options={options} 
+                      placeholder="Employee Working Projects"
+                    />
+                  <div className="invalid-feedback">Please enter a bsr code.</div>
+                </div>
+              </div>
+              <div className="row mb-3 align-items-center">
+                <div className="col-lg-1 fw-medium">BM Unit:</div>
+                <div className="col-lg-3">
+                    <Select 
+                      options={options} 
+                      placeholder="Employee Working Projects"
+                    />
+                  <div className="invalid-feedback">Please enter a bsr code.</div>
+                </div>
+                <div className="col-lg-1 fw-medium">BM Unit Text:</div>
+                <div className="col-lg-3">
                   <input
                     type="text"
                     className="form-control form-control-sm p-2"
-                    placeholder="Tool Name"
-                    name="tool_name"
-                    value={formData.tool_name}
-                    onChange={handleOnChange}
+                    placeholder="BM Unit Text"
+                    name="bm_Unit_text"
                     required
                   />
-                  <div className="invalid-feedback">Please enter a tool name.</div>
                 </div>
-              </div>
-
-              <div className="row mb-3">
-                <div className="col">
-                  <Select
-                    options={options}
-                    placeholder="Select Unit"
-                    onChange={handleUnitChange}
-                    className={validate && !formData.unit ? 'is-invalid' : ''}
-                  />
-                  {validate && !formData.unit && (
-                    <div className="invalid-feedback d-block">Please select a unit.</div>
-                  )}
-                </div>
-
-                <div className="col">
+                <div className="col-lg-1 fw-medium">BM Unit Num:</div>
+                <div className="col-lg-3">
                   <input
                     type="text"
                     className="form-control form-control-sm p-2"
-                    placeholder="Tool Rate"
-                    name="rate"
-                    value={formData.rate}
-                    onChange={handleOnChange}
+                    placeholder="BM Unit Number"
+                    name="bm_Unit_text"
                     required
                   />
-                   <div className="invalid-feedback">Please enter Tool type.</div>
                 </div>
               </div>
+              {Array.from({ length: materialCount }).map((_, index) => (
+                <div 
+                  className="row mb-2"
+                  key={index}
+                >
+                  <div className="col">
+                    <div className="po-container pt-3 pb-3 ps-4 pe-4 rounded-2 site-border h-100">
+                      <div className="row align-items-center mb-3">
+                        <div className="col-lg-10">material - {index}</div>
+                        <div className="col-lg-2 text-end">
+                          <button 
+                            className="btn btn-sm btn-outline-danger" 
+                            type="button"
+                            onClick={() => SetMaterialCount(materialCount - 1)}
+                            disabled={materialCount <= 1}
+                          >
+                            <i className="bi bi-x-lg"></i>
+                          </button>
+                        </div>
+                      </div>
+                      <div className="row mb-2">
+                        <div className="col">
+                          <Select 
+                            options={options} 
+                            placeholder="Ref Type"
+                          />
+                        </div>
+                        <div className="col">
+                          <Select 
+                            options={options} 
+                            placeholder="Ref Number"
+                          />
+                        </div>
+                        <div className="col">
+                          <Select 
+                            options={options} 
+                            placeholder="Unites"
+                          />
+                        </div>
+                        <div className="col">
+                          <input
+                            type="text"
+                            className="form-control form-control-sm p-2"
+                            placeholder="Unit Rate"
+                            required
+                          />
+                        </div>
+                        <div className="col">
+                          <input
+                            type="text"
+                            className="form-control form-control-sm p-2"
+                            placeholder="Quantity"
+                            required
+                          />
+                        </div>
+                      </div>
+                      <div className="row mb-2">
+                        <div className="col">
+                          <Select 
+                            options={options} 
+                            placeholder="Ref Type"
+                          />
+                        </div>
+                        <div className="col"></div>
+                        <div className="col"></div>
+                        <div className="col"></div>
+                        <div className="col"></div>
+                      </div>
+                      <div className="row mb-2">
+                        <div className="col">
+                          <Select 
+                            options={options} 
+                            placeholder="Ref Type"
+                          />
+                        </div>
+                        <div className="col"></div>
+                        <div className="col"></div>
+                        <div className="col"></div>
+                        <div className="col"></div>
+                      </div>
+                      <div className="row">
+                        <div className="col">
+                          <Select 
+                            options={options} 
+                            placeholder="Ref Type"
+                          />
+                        </div>
+                        <div className="col">
+                          <Select 
+                            options={options} 
+                            placeholder="Ref Number"
+                          />
+                        </div>
+                        <div className="col">
+                          <Select 
+                            options={options} 
+                            placeholder="Unites"
+                          />
+                        </div>
+                        <div className="col">
+                          <input
+                            type="text"
+                            className="form-control form-control-sm p-2"
+                            placeholder="Unit Rate"
+                            required
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+              
 
-              <div className="row">
-                <div className="col text-center">
-                  <button className="btn btn-sm btn-warning w-25" type="submit">
-                    Submit
+              <div className="row mb-3">
+                <div className="col text-end">
+                  <button 
+                    onClick={() => SetMaterialCount(materialCount + 1)}
+                    type='button'
+                    class="btn btn-sm add-new-line-btn"
+                  >
+                    <i class="bi bi-plus-square me-2 color-purple"></i><span class="nav-font-color">Add new line</span>
                   </button>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col">
+                  <textarea name="" id="" className="form-control" placeholder="BSR Description"></textarea>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col text-center mt-3">
+                  <button className="btn btn-warning w-25">Submit</button>
                 </div>
               </div>
             </form>
